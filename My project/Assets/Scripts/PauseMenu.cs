057edf201b2255d49381e7adf682f5db8cf7f9da
MattCastUCM <mattcast@ucm.es>
2024-03-16 05:32:59 +0100

    Cuenta atras al salir de pausa, meter TextMeshPro

--- My project/Assets/Scripts/PauseMenu.cs
+++ My project/Assets/Scripts/PauseMenu.cs
@@ -1,22 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
 
-    [SerializeField] private GameObject _pauseMenu;
+    [SerializeField]  GameObject _pauseMenu;
 
+    [SerializeField] private float _countdownTime = 3.5f;
+    [SerializeField] private GameObject _countdown;
+    TextMeshProUGUI _countdownText = null;
+    bool exiting;
+    float counter;
+    
     public void enterPause()
     {
+        exiting = false;
         _pauseMenu.SetActive(true);
+        _countdown.SetActive(false);
         Time.timeScale = 0;
     }
     public void quitPause()
     {
         _pauseMenu.SetActive(false);
-        Time.timeScale = 1;
+        _countdown.SetActive(true);
+        exiting = true;
+
+        // + 1 porque el contador muestra su valor truncado,
+        // y nada mas comienza la cuenta atras, _countdownTime
+        // deja de ser el numero indicado y se pasa a mostrar
+        // el entero anterior
+        counter = _countdownTime + 1;
+        
     }
 
     private void setPause()
@@ -35,11 +52,30 @@ public class PauseMenu : MonoBehaviour
     void Start()
     {
         quitPause();
+
+        _countdownText = _countdown.GetComponent<TextMeshProUGUI>();
+        _countdown.SetActive(false);
+        exiting = false;
+        Time.timeScale = 1;
     }
 
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape)) setPause();
+
+        if (exiting)
+        {
+            _countdownText.text = ((int)counter).ToString();
+            counter -= Time.unscaledDeltaTime;
+
+            if (counter < 1)
+            {
+                _countdown.SetActive(false);
+                exiting = false;
+                Time.timeScale = 1;
+                Debug.Log("Unpaused");
+            }
+        }
     }
 }
